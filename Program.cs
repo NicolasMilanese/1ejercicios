272d70813cd7d09e3620ab84d568b89f1ff8d89e
NicolasMilanese <nicolasmilanese2004@gmail.com>
2022-10-13 21:07:29 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ejercicio
+{
+class Program
+{
+static void Main (string[] args)
+{
+Console.Clear();
+
+Console.WriteLine("Paso 1: calentar el agua.");
+Console.ReadKey();
+Console.WriteLine("Paso 2: Colocar café a gusto en la taza.");
+Console.ReadKey();
+Console.WriteLine("Paso 3: Echar el agua caliente.");
+Console.ReadKey();
+Console.WriteLine("Paso 4: Revolver.");
+Console.ReadKey();
+Console.WriteLine("Paso 5: Agregar azucar o endulzante a gusto.");
+Console.ReadKey();
+Console.WriteLine("Paso 6: Tomar.");
+Console.ReadKey();
+}
+
+}
+
+}
